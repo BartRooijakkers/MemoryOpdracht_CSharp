ce7f4b0a3169f0a4bcd9de140370649b28ff7504
Bart Rooijakkers <bart_rooyakkers@hotmail.com>
2025-12-04 16:59:21 +0100

    Finished!
    Finally i finished this shit

--- MemoryGame.Wpf/ViewModels/GameViewModel.cs
+++ MemoryGame.Wpf/ViewModels/GameViewModel.cs
@@ -9,6 +9,8 @@ using MemoryGame.Wpf;               // RelayCommand
 using MemoryGame.Wpf.ViewModels;    // CardViewModel
 
 using System.Diagnostics;
+using MemoryGame.Data.Repositories;
+using MemoryGame.Domain.Interfaces;
 
 
 namespace MemoryGame.Wpf.ViewModels
@@ -17,8 +19,32 @@ namespace MemoryGame.Wpf.ViewModels
     {
         private readonly Game _game = new(); // Underlying game model
         private readonly DispatcherTimer _uiTimer; // Timer to update elapsed time in UI
+        private readonly IHighScoreRepository _highScoreRepo = new JsonHighScoreRepository(); // repository for high scores
 
         public ObservableCollection<CardViewModel> Cards { get; } = new(); // Collection of card view models for data binding   
+
+        //Paircount property to allow user selection of pair count
+        private int _pairCount = 5; // (default) Number of card pairs
+        public int PairCount
+        {
+            get => _pairCount;
+            set
+            {
+                _pairCount = value; OnPropertyChanged(); // Notify UI of changes
+            } 
+        }
+        //Player name property to allow user input for player name
+        private string _playerName = "Bart";
+        public string PlayerName
+        {
+            get => _playerName;
+            set { _playerName = value; OnPropertyChanged(); }
+        }
+
+        // High scores collection for data binding - loaded from repository.
+        // This will allow me to bind the high scores to a UI element.
+        public ObservableCollection<HighScoreEntry> HighScores { get; } = new();
+
 
         private int _attempts;// Number of attempts made
         public int Attempts
@@ -49,10 +75,12 @@ namespace MemoryGame.Wpf.ViewModels
         {
             // UI-timer to update elapsed time every 250 ms
             _uiTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(250) }; // Set interval
-            _uiTimer.Tick += (_, __) => ElapsedText = Format(_game.ElapsedTime); // Update elapsed time text
+            _uiTimer.Tick += (_, __) => ElapsedText = Format(_game.ElapsedTime); // Update elapsed time text -- Hoogie
 
             StartCommand = new RelayCommand(_ => StartGame()); // Command to start a new game
             FlipCardCommand = new RelayCommand(param => FlipCard(param)); // Command to flip a card
+
+            LoadHighScores(); // Load high scores from repository
         }
 
         /// <summary>
@@ -61,10 +89,9 @@ namespace MemoryGame.Wpf.ViewModels
         /// <remarks>This method initializes the game state, clears any existing cards, and sets up the
         /// game timer.  The game begins immediately after this method is called.</remarks>
         /// <param name="pairCount">The number of card pairs to include in the game. The default value is 5.</param>
-        private void StartGame(int pairCount = 5)
+        private void StartGame()
         {
-            //TODO: Allow user to select pair count from textbox.
-            _game.Start(pairCount); // Start the game in the model
+            _game.Start(PairCount); // Start the game in the model
 
             Cards.Clear(); // Clear existing cards
             foreach (var c in _game.Cards) // Iterate over model cards
@@ -142,7 +169,20 @@ namespace MemoryGame.Wpf.ViewModels
             {
                 _uiTimer.Stop();
                 Trace.WriteLine("Game completed! Stopping timer."); // Debug output
-                Trace.WriteLine($"Score: {_game.CalculateScore()}");
+
+                var (added, rank, entry) = _game.SaveHighScore(_highScoreRepo, PlayerName);
+
+                LoadHighScores(); // Reload high scores from repository, If new score has been added it will show up
+
+                if (added)
+                {
+                    Trace.WriteLine($"Highscore! Rank: {rank}, Score: {entry.Score}");
+                }
+                else
+                {
+                    Trace.WriteLine("No highscore achieved.");
+                }
+
             }
         }
 
@@ -156,5 +196,14 @@ namespace MemoryGame.Wpf.ViewModels
         public event PropertyChangedEventHandler? PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string? n = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(n));
+
+        // Load high scores from repository
+        private void LoadHighScores()
+        {
+            HighScores.Clear();
+            foreach (var hs in _highScoreRepo.GetTop10())
+                HighScores.Add(hs);
+        }
+
     }
 }
